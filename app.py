9fc760c20f9055b1a1a6b67f4a02d3fb66f59db7
Jeff Liu <Jeff.Liu@nz.ey.com>
2025-04-28 14:39:50 +1200

    st 显示 df 带表头，显示 raws 不带表头

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import streamlit as st
 
 db='postgres'
 sc=jfutils.dbutils.DBUtils(db,st.secrets[db]['server'],st.secrets[db]['port'],st.secrets[db]['user'],st.secrets[db]['password'],st.secrets[db]['database'])
-data=sc.get_query_rows('select * from tcommodity limit 10')
+data=sc.get_query_df('select * from tcommodity limit 10')
 st.title('Streamlit App with PostgreSQL')
 st.write('This is a simple Streamlit app that connects to PostgreSQL and displays data.')
 st.write('Data from PostgreSQL:')
-if data:
+if not data.empty:
     st.table(data)
